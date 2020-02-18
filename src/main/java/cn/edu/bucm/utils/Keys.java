652448825adb0f9c134271b6f57ebef4981f8d66
247707798 <liuzhe@bucm.edu.cn>
2020-02-18 12:24:59 +0800

    test111

--- src/main/java/cn/edu/bucm/utils/Keys.java
+++ src/main/java/cn/edu/bucm/utils/Keys.java
@@ -3,6 +3,6 @@ package cn.edu.bucm.utils;
 public class Keys {
 
     public static String REDIS_IMG_LIST_NAME = "imgList";
-    public static String test = "111";
+    public static String test = "222";
 
 }
